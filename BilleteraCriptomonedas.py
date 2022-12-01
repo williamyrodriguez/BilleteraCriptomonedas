9af0071d831b31636bab396d72350b1f5949a20d
William Rodriguez <101745645+williamyrodriguez@users.noreply.github.com>
2022-12-01 12:16:07 -0500

    Update BilleteraCriptomonedas.py

--- BilleteraCriptomonedas.py
+++ BilleteraCriptomonedas.py
@@ -1,25 +1,26 @@
+#Importaciones necesarias
 import requests
 from datetime import date
 
-#Testing if menu actio is correct
+#Probar si la entrada del menú es correcta
 def esopcion (menu):
   return esnumeroentero(menu) and 7>int(menu)>0
-      
+#Prueba si la moneda está dentro de coinmarketcap.com      
 def esmoneda(cripto):
     return cripto in monedas
-
+#Verifica si una entrada es un número natural
 def esnumero(numero): 
     return numero.replace('.','',1).isdigit()
-  
+#Verifica si una entrada es un número entero 
 def esnumeroentero(numero): 
     return numero.isdigit()
-
+#Verifica si el codigo del remitente o destinatario de las transacciones es correcto
 def escodigo(numero): 
     return esnumeroentero(numero) and int(numero)!=codigo_propio
-
+#Verifica si la moneda relacionada a la accion está en el balance de la cuenta
 def monedaEnBalance(moneda):
   return moneda in monedas_balance
-
+#Funcion para validar la moneda ingresada por el usuario
 def PreguntarMoneda(tipoTransaccion):
   moneda=input('Indique el nombre de la moneda a '+ tipoTransaccion+':' )
   while not esmoneda(moneda):
@@ -28,7 +29,7 @@ def PreguntarMoneda(tipoTransaccion):
       print('')
       moneda=input('Indique el nombre de la moneda a '+ tipoTransaccion+':' )
   return moneda
-
+#Función para validar la cantidad de la moneda ingresada por el usuario
 def PreguntarCantidad(moneda):
   cantidad = input('Indique la cantidad de '+moneda+':')
   while not esnumero(cantidad):
@@ -37,7 +38,7 @@ def PreguntarCantidad(moneda):
     print('')
     cantidad = input("Indique la cantidad de "+moneda+":")
   return cantidad
-
+#Función para validar el código del emisor o receptor de la transacción ingresada por el usuario
 def PedirCodigo(tipo_receptor):
   codigo = input('Indique código del '+ tipo_receptor+ ' (Este es un valor numérico entero):')
   while not escodigo(codigo):
@@ -46,7 +47,7 @@ def PedirCodigo(tipo_receptor):
     print('')
     codigo = input('Indique código del '+ tipo_receptor+ ' valido:')
   return codigo
-    
+#Función para registrar las transacciones de la cuenta
 def RegistrarTransaccion(moneda,tipo,codigo,cantidad,monto):
     today=date.today()
     transaccion=[]
@@ -58,6 +59,7 @@ def RegistrarTransaccion(moneda,tipo,codigo,cantidad,monto):
     transaccion.append(monto)
     transacciones.append(transaccion)
     
+#Función para recibir una transacción   
 def RecibirTransaccion():
   print('')
   moneda=PreguntarMoneda('recibir')  
@@ -77,7 +79,8 @@ def RecibirTransaccion():
     print('')
     print('Se agregaron a la cuenta ',cantidad,'', moneda)
     print('')
-
+    
+#Función para realizar una transacción de envío
 def TransferirMonto():
   print('')
   moneda=PreguntarMoneda('transferir')
@@ -99,10 +102,10 @@ def TransferirMonto():
       print('')
       monedas_balance_dic[moneda]=monedas_balance_dic[moneda]-float(cantidad)
       if monedas_balance_dic[moneda]==0:
-        del monedas_balance_dic[moneda]
-        
+        del monedas_balance_dic[moneda]       
       RegistrarTransaccion(moneda,'Envío    ',codigo,cantidad,monedas_dic[moneda])
-  
+      
+#Función para mostrar el balance de una moneda especificada por el usuario
 def MostrarBalanceMoneda():
   print('')
   moneda = moneda=PreguntarMoneda('consultar')
@@ -117,35 +120,37 @@ def MostrarBalanceMoneda():
   else:
     print('No posee esta moneda en su portafolio!')
     print('')
-
+    
+#Función para mostrar el balance general de la cuenta
 def MostrarBalanceGeneral():
   print('')
   print('Balance general')
   print('')
-  print('Nombre   Cantidad   Valor en USD     Total Moneda en USD')
+  print('Nombre     Cantidad   Valor en USD     Total Moneda en USD')
   total_USD=0
   for moneda in monedas_balance_dic:
     USDequivalente=monedas_dic[moneda] * monedas_balance_dic[moneda]
-    print (moneda, '       ',monedas_balance_dic[moneda], '       %10.3f'%monedas_dic[moneda]+ '     %15.3f'%USDequivalente   )
+    print (moneda, '       %8.3f'%monedas_balance_dic[moneda], '   %10.3f'%monedas_dic[moneda]+ '     %15.3f'%USDequivalente   )
     total_USD+=USDequivalente
   print('')
   print('USD Totales: $%15.3f'%total_USD+' USD')
   print('')
     
-
+#Función para mostrar el historial de transacciones de la cuenta
 def MostrarTransacciones():
   print('')
   print('Historial de Transacciones')
   print('')
-  print('Fecha        Moneda   Tipo de Operación  Código de usuario   Cantidad    USD al momento')
+  print('Fecha        Moneda   Tipo de Operación  Código usuario       Cantidad        USD al momento')
   for registro in transacciones:
-    print (registro[0], '   ',registro[1], '       ',registro[2], '        ',registro[3] , '               ',registro[4], '   %15.3F '%registro[5]  )
+    print (registro[0], '   ',registro[1], '       ',registro[2], '        ',registro[3] , '               %8.3f'%float(registro[4])+ '   %15.3f '%float(registro[5])  )
 
   print('')
   
 #Asignación del codigo del usuario
 codigo_propio=132
 
+#Creación de variables a utilizar
 monedas=()
 monedas_dic={}
 monedas_balance=()
@@ -184,7 +189,7 @@ while 1:
   menu=input("Ingrese el número de la acción que desea realizar:");
   while not esopcion(menu):
     menu=input("Acción NO VALIDA!. Ingrese el número de la acción que desea realizar:");
-
+  #Selección de función de acuerdo a la entrada del usuario
   if menu=="1":
     RecibirTransaccion() 
 
@@ -204,6 +209,5 @@ while 1:
     print('')
     print('Se cerró la apricación, ¡Hasta Pronto!')
     break
-
 
 
